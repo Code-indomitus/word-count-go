fb4a83278fdfe2d9af05cefb95c527575708a19c
Shyam Kamalesh Borkar <shyam.borkar108@gmail.com>
2024-05-30 22:57:07 +0800

    Completed Step 1 to 4, small problem with matching multibyte character counts for step 4.

--- word-count-go/wc.go
+++ word-count-go/wc.go
@@ -5,20 +5,29 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Declare the flag options of word count
 var byteCountFlag bool
+var lineCountFlag bool
+var wordCountFlag bool
+var charCountFlag bool
 
 func main() {
 	// Parse command line arguments for flags
 	flag.BoolVar(&byteCountFlag, "c", false, "print the byte counts")
-	flag.BoolVar(&byteCountFlag, "bytes", false, "print the byte counts")
+	flag.BoolVar(&lineCountFlag, "l", false, "print the newline counts")
+	flag.BoolVar(&wordCountFlag, "w", false, "print the word counts")
+	flag.BoolVar(&charCountFlag, "m", false, "print the character counts")
+
+	// flag.BoolVar(&byteCountFlag, "bytes", false, "print the byte counts") TODO remove this later
 	flag.Parse()
 
-	fmt.Println(len(os.Args))
+	//TODO: Remove this later
+	// fmt.Println(len(os.Args))
 
-	fmt.Println("byteCountFlag:", byteCountFlag)
+	// fmt.Println("byteCountFlag:", byteCountFlag)
 
 	// Get the file name to be read
 	fileName := os.Args[len(os.Args)-1]
@@ -30,15 +39,35 @@ func main() {
 		return
 	}
 
+	// Close the file later
 	defer file.Close()
 
+	// Execute the appropriate functions based on the flags
+	if byteCountFlag {
+		printByteCount(file, fileName)
+	}
+
+	if lineCountFlag {
+		printLineCount(file, fileName)
+	}
+
+	if wordCountFlag {
+		printWordCount(file, fileName)
+	}
+
+	if charCountFlag {
+		printCharCount(file, fileName)
+	}
+}
+
+func printByteCount(file *os.File, fileName string) {
 	// Read file as a buffer
 	reader := bufio.NewReader(file)
 
 	var byteCount int64 = 0
 
 	for {
-		// Read a chunk of 1kB of data from the file
+		// Read a chunk of 1kB of data from the file at at time
 		chunk := make([]byte, 1024)
 		bytesRead, err := reader.Read(chunk)
 		if err != nil && err.Error() != "EOF" {
@@ -55,10 +84,53 @@ func main() {
 		byteCount += int64(bytesRead)
 	}
 
-	// // Check which flags have been set
-	// for index, value := range os.Args {
-	// 	// Print file name
-	// 	fmt.Println("File", index, ":", value)
-	// }
 	fmt.Println(byteCount, fileName)
+}
+
+func printLineCount(file *os.File, fileName string) {
+	// Initialize file scanner
+	fileScanner := bufio.NewScanner(file)
+
+	var lineCount int64 = 0
+
+	// Iterate over each line in the file and increment the line count
+	for fileScanner.Scan() {
+		lineCount++
+	}
+
+	fmt.Println(lineCount, fileName)
+}
+
+func printWordCount(file *os.File, fileName string) {
+	// Initialize file scanner
+	fileScanner := bufio.NewScanner(file)
+
+	var wordCount int64 = 0
+
+	// Iterate over each line and get number of words
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		// Increment word count by number of words in the line
+		wordCount += int64(len(strings.Fields(line)))
+	}
+
+	fmt.Println(wordCount, fileName)
+}
+
+func printCharCount(file *os.File, fileName string) {
+	// Initialize file scanner
+	fileScanner := bufio.NewScanner(file)
+
+	var charCount int64 = 0
+
+	// Iterate over each line and get number of characters
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		// Increment word count by number of words in the line
+		charCount += int64(len(line))
+	}
+
+	fmt.Println(charCount, fileName)
 }
